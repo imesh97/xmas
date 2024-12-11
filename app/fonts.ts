7ceb03e6419b6c2f05dc53cf7aff0e28b4982a0f
imesh <imesh.nimsitha@gmail.ca>
2024-12-10 23:41:30 -0500

    updated fonts

---
+++ app/fonts.ts
@@ -0,0 +1,12 @@
+import { Fredoka, Pacifico } from "next/font/google";
+
+export const fredoka = Fredoka({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const pacifico = Pacifico({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400"],
+});
