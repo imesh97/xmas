8f9858a08d9560a5578a651879127e45030938f4
imesh <imesh.nimsitha@gmail.ca>
2024-12-10 23:40:34 -0500

    installed dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,11 +10,22 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "#f5eed7",
+        foreground: "#141414",
+        re: "#FF597B",
+        sa: "#FF8E9E",
+        pi: "#F68EB7",
+        "pi-la": "#FCA2B7",
+        la: "#F9B5D0",
+        gr: "#EEEEEE",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
